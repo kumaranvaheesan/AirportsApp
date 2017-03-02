4c953ffeed90c1c6f90c62e51403f6b616d01b37
kumaran V <kumaran@uptech.com.au>
2017-03-02 13:38:14 +1100

    Initial Commit

---
+++ Airports AppTests/Airports_AppTests.swift
@@ -0,0 +1,36 @@
+//
+//  Airports_AppTests.swift
+//  Airports AppTests
+//
+//  Created by kumaran V on 02/03/17.
+//  Copyright © 2017 Kumaran. All rights reserved.
+//
+
+import XCTest
+@testable import Airports_App
+
+class Airports_AppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
